db0d4ff6382c7d6f754fe22bb3ec7ce44092bcf6
Kuba Twardowski <kuba@replient.com>
2023-12-27 18:27:18 -0600

    matt tinder

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,16 +11,53 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
 
+    #[Route('/tinder', name: 'page_kubas')]
+    public function tinder2()
+    {
+        return $this->render('Description.html.twig');
 
-
-    #[Route('/kuba', name: 'page_kuba_test')]
-    public function testPage(Request $request) {
-
-        return $this->render('base.html.twig');
     }
 
-    private function helperFucntion() {
-        
+    #[Route('/hobbies', name: 'matts_hobbies')]
+    public function hobbies()
+    {
+        return $this->render('mattsHobbies.html.twig');
+    }
+
+
+    #[Route('/', name: 'page_kuba_test')]
+    public function testPage(Request $request, EntityManagerInterface $entityManager)
+    {
+
+        // Create Object
+        $message = new Message();
+        $message->setMessageData("Swipe right please!");
+
+        // Save Object to DB
+        $entityManager->persist($message);
+        $entityManager->flush();
+
+
+        // Get All the "Messages"
+        $message = $entityManager->getRepository(Message::class)->findAll();
+
+        // Render the First Page with all the "Messages"
+        return $this->render('default.html.twig', array(
+            'messages' => $message
+        ));
+    }
+
+    #[Route('/kuba2/{id}', name: 'page_kuba_test2')]
+    public function testPage2(Request $request, EntityManagerInterface $entityManager, $id)
+    {
+        $message = $entityManager->getRepository(Message::class)->find($id);
+        return $this->render('Controversial Topics.html.twig', array("message" => $message));
+    }
+
+
+    private function helperFucntion()
+    {
+
     }
 
 }
