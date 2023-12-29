875515bdfb2768e00894c4f6e42a8e22137e1a14
Kuba Twardowski <kuba@replient.com>
2023-12-28 18:37:36 -0600

    javascirpt practice

---
+++ public/Description.js
@@ -0,0 +1,17 @@
+let person = {
+    name: 'matthew',
+    DOB: 'O6-24-97',
+    Height: '6-2',
+    SMV_value: 7.0,
+};
+console.log = function (person) {
+    
+};
+console.log(person);
+
+let certainType = ['blonde', 'fit',];
+
+certainType[2] = '5-8=<';
+
+let console;
+console.log(certainType);
